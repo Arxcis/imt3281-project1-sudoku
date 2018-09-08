f14f268bb97ec9d6a844baa3bfbfaf63d16278fe
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2018-09-08 22:45:41 +0200

    fix #22: Create internal sudoku representation
    Went for a 2D arraylist for the representation of the sudoku board, to
    avoid having to calculate the positions of the cells.

--- src/no/ntnu/imt3281/sudoku/Sudoku.java
+++ src/no/ntnu/imt3281/sudoku/Sudoku.java
@@ -1,5 +1,20 @@
 package no.ntnu.imt3281.sudoku;
 
-public class Sudoku {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Sudoku {
+    private List<List<Integer>> mSudokuBoard;
+
+    /**
+     * Creates a new sudoku board from the given board
+     * 
+     * @param board The new board
+     */
+    public Sudoku(final List<List<Integer>> board) {
+        mSudokuBoard = new ArrayList<>();
+        for (var row : board) {
+            mSudokuBoard.add(new ArrayList<>(row));
+        }
+    }
 }
