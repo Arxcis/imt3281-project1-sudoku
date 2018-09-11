f3d952c5a848366498a3a55b119c81ef88de6a4d
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2018-09-11 22:22:53 +0200

    ref #27: Add tests to empty sudoku constructor

--- test/no/ntnu/imt3281/sudoku/SudokuTest.java
+++ test/no/ntnu/imt3281/sudoku/SudokuTest.java
@@ -6,9 +6,15 @@ import org.junit.Test;
 
 public class SudokuTest {
 
-	@Test
-	public void testEmptyConstructor() {
-		Sudoku sudoku = new Sudoku();
-		assertTrue(sudoku instanceof Sudoku);
-	}
+    @Test
+    public void testEmptyConstructor() {
+        Sudoku sudoku = new Sudoku();
+        assertTrue(sudoku instanceof Sudoku);
+        
+        for (int row = 0; row < Sudoku.ROW_SIZE; row++) {
+            for (int col = 0; col < Sudoku.COL_SIZE; col++) {
+                assertTrue(sudoku.getElement(row, col) == Sudoku.EMPTY_CELL);
+            }
+        }
+    }
 }
