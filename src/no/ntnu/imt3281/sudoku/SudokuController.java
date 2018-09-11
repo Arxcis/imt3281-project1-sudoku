d313ae28e06007ada35259736e89b9ac1e06cd3b
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-09-12 00:15:32 +0200

    ref #28: drawing simple rectangles

--- src/no/ntnu/imt3281/sudoku/SudokuController.java
+++ src/no/ntnu/imt3281/sudoku/SudokuController.java
@@ -1,15 +1,18 @@
 package no.ntnu.imt3281.sudoku;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 
 public class SudokuController {
     
@@ -18,19 +21,25 @@ public class SudokuController {
 		return FXMLLoader.load(SudokuController.class.getResource(resourcePath));
     }
 
-    @FXML
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnNewGame"
     private Button btnNewGame;
 
-    @FXML
+    @FXML // fx:id="btnSave"
     private Button btnSave;
 
-    @FXML
+    @FXML // fx:id="btnLoad"
     private Button btnLoad;
 
-    @FXML
+    @FXML // fx:id="btnExit"
     private Button btnExit;
 
-    @FXML
+    @FXML // fx:id="gridSudoku"
     private GridPane gridSudoku;
 
     @FXML
@@ -56,5 +65,26 @@ public class SudokuController {
     @FXML
     void OnMousePressedGrid(MouseEvent event) {
     	System.out.println("OnMousePressedGrid");
+    }
+
+    @FXML
+    void initialize() {
+        assert btnNewGame != null : "fx:id=\"btnNewGame\" was not injected: check your FXML file 'sudoku.fxml'.";
+        assert btnSave != null : "fx:id=\"btnSave\" was not injected: check your FXML file 'sudoku.fxml'.";
+        assert btnLoad != null : "fx:id=\"btnLoad\" was not injected: check your FXML file 'sudoku.fxml'.";
+        assert btnExit != null : "fx:id=\"btnExit\" was not injected: check your FXML file 'sudoku.fxml'.";
+        assert gridSudoku != null : "fx:id=\"gridSudoku\" was not injected: check your FXML file 'sudoku.fxml'.";
+        
+        for (int row = 0; row < this.gridSudoku.getRowCount(); ++row) {
+        	for (int col = 0; col < this.gridSudoku.getColumnCount(); ++col) {
+        		var rect = new Rectangle(44,42);
+        		rect.setFill(Color.TRANSPARENT);
+        		rect.setStrokeType(StrokeType.INSIDE);
+        		rect.setStroke(Color.BLACK);
+                rect.setStrokeWidth(1);
+            
+        		this.gridSudoku.add(rect, col, row);
+        	} 
+        }
     }
 }
