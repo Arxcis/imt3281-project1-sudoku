ab67d975c4f6a7497cf74bba5afdf3799e53e7ea
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2018-09-13 20:22:21 +0200

    ref #8: Add new online test case
    Added another test case for the json parsing. This one was taken from an
    online sudoku site, so should be a correct sudoku (the supplied one did
    not go through an online solver, indicating it might not be 100% correct?)

--- test/no/ntnu/imt3281/sudoku/SudokuTest.java
+++ test/no/ntnu/imt3281/sudoku/SudokuTest.java
@@ -18,8 +18,11 @@ public class SudokuTest {
         }
     }
 
+    /**
+     * Tests if we can parse the sudoku supplied with the project.
+     */
     @Test
-    public void testJSONParseSuccess() {
+    public void testJSONParseSuccessSuppliedSudoku() {
         var string = "[[5, 3, -1, -1, 7, -1, -1, -1, -1],\n" +
                 "[6, -1, -1, 1, 9, 5, -1, -1, -1], \n" +
                 "[-1, 9, 8, -1, -1, -1, -1, 6, -1], \n" +
@@ -39,6 +42,44 @@ public class SudokuTest {
                 {-1, 6, -1, -1, -1, -1, 2, 8, -1},
                 {-1, -1, -1, 4, 1, 9, -1, -1, 5},
                 {-1, -1, -1, -1, 8, -1, -1, 7, 9}};
+
+        var sudoku = Sudoku.loadSudokuFromJson(string);
+        for (int row = 0; row < desired.length; row++) {
+            for (int col = 0; col < desired[row].length; col++) {
+                assertTrue(sudoku.getElement(row, col) == desired[row][col]);
+            }
+        }
+    }
+
+
+    /**
+     * Tests if we can parse a sudoku found online, mainly to avoid having just one test case.
+     */
+    @Test
+    public void testJSONParseSuccessOnlineSudoku() {
+        var string = "[\n" +
+                "[-1, -1, -1, 1, -1, -1, 5, 9, -1],\n" +
+                "[-1, -1, 1, -1, 9, -1, -1, 8, 4],\n" +
+                "[-1, -1, 8, -1, 3, -1, 7, -1, 6],\n" +
+                "[8, 3, -1, 9, 5, -1, -1, -1, -1],\n" +
+                "[-1, 5, 6, 3, -1, 1, 8, 7, -1],\n" +
+                "[-1, -1, -1, -1, 4, 6, -1, 3, 5],\n" +
+                "[1, -1, 3, -1, 7, -1, 4, -1, -1],\n" +
+                "[6, 8, -1, -1, 1, -1, 9, -1, -1],\n" +
+                "[-1, 4, 9, -1, -1, 5, -1, -1, -1]\n" +
+                "]";
+
+        int[][] desired = {
+                {-1, -1, -1, 1, -1, -1, 5, 9, -1},
+                {-1, -1, 1, -1, 9, -1, -1, 8, 4},
+                {-1, -1, 8, -1, 3, -1, 7, -1, 6},
+                {8, 3, -1, 9, 5, -1, -1, -1, -1},
+                {-1, 5, 6, 3, -1, 1, 8, 7, -1},
+                {-1, -1, -1, -1, 4, 6, -1, 3, 5},
+                {1, -1, 3, -1, 7, -1, 4, -1, -1},
+                {6, 8, -1, -1, 1, -1, 9, -1, -1},
+                {-1, 4, 9, -1, -1, 5, -1, -1, -1}
+                };
 
         var sudoku = Sudoku.loadSudokuFromJson(string);
         for (int row = 0; row < desired.length; row++) {
