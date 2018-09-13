558dcf5d8d576ce94d7923215f99aeab9ca6f09f
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-09-13 23:31:31 +0200

    ref #34: merge master + bugfix
    - Running the stop function to cleanup, wait to shut down all
    javafx threads.

--- src/no/ntnu/imt3281/sudoku/Main.java
+++ src/no/ntnu/imt3281/sudoku/Main.java
@@ -1,6 +1,10 @@
 package no.ntnu.imt3281.sudoku;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -17,7 +21,22 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 
+    // @ref Stop() cleanup - https://stackoverflow.com/a/46060236 - 13.09.18    
+    private final ExecutorService exec = Executors.newCachedThreadPool();
+
+    @Override
+    public void stop() throws InterruptedException {
+        System.out.println("Stop called: try to let background threads complete...");
+        exec.shutdown();
+        if (exec.awaitTermination(2, TimeUnit.SECONDS)) {
+            System.out.println("Background threads exited");
+        } else {
+            System.out.println("Background threads did not exit, trying to force termination (via interruption)");
+            exec.shutdownNow();
+        }       
+    }
+
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
