7c43f333fbe71edcf3f6c12f19379d6b070cb2dc
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-09-27 21:12:37 +0200

    PR #13: using OS default locale

--- src/no/ntnu/imt3281/sudoku/Language.java
+++ src/no/ntnu/imt3281/sudoku/Language.java
@@ -7,29 +7,18 @@ import java.util.ResourceBundle;
  * Manage Language strings. Access from anywhere.
  */
 public class Language {
-    
     /*
-     * ISO Locale language
-     */
-    private static final String language = "nb"; // @TODO Get from config file
-    
-    /*
-     * ISO Locale Country
-     */
-    private static final String country = "NO"; // @TODO Get from config file
-    
-    /*
-     * Path to bundle - a bunch of files starting with same name, ending on locale string
+     * Path to language bundle
      */
     private static final String bundlePath = "no.ntnu.imt3281.sudoku.LanguageBundle"; // @TODO Get from config file
     
     /*
-     * Resource bundle differs based on the configured locale
+     * Resource bundle differs based on default locale
      */
     private static ResourceBundle bundle;
 
     public static void init() {
-        Language.bundle = ResourceBundle.getBundle(Language.bundlePath, new Locale(Language.language, Language.country) );
+        Language.bundle = ResourceBundle.getBundle(Language.bundlePath);
     }
     
     /**
