71df207725392c6d88f2bed053705c4cf06c918b
Per-Morten Straume <p.m.straume@outlook.com>
2018-09-29 20:08:53 +0200

    ref #48: Document exception classes
    Added documentation to exception classes as highlighted by SonarCube.

--- src/no/ntnu/imt3281/sudoku/BadNumberException.java
+++ src/no/ntnu/imt3281/sudoku/BadNumberException.java
@@ -1,9 +1,24 @@
 package no.ntnu.imt3281.sudoku;
 
+/**
+ * BadNumberException is a sudoku specific exception indicating that the
+ * specified value one is trying to add to a sudoku board is invalid. i.e. it
+ * already exists within the row/column or sub-grid
+ *
+ * @see no.ntnu.imt3281.sudoku.Sudoku#addNumber(int, int, int)
+ */
 @SuppressWarnings("serial")
 public class BadNumberException extends IllegalArgumentException {
     final SudokuIterator mIterator;
 
+    /**
+     * Creates a BadNumberException holding the iterator argument.
+     *
+     * @param it The iterator to the first element found which contains the value
+     *           that one is trying to duplicate.
+     *
+     * @see no.ntnu.imt3281.sudoku.Sudoku#addNumber(int, int, int)
+     */
     public BadNumberException(SudokuIterator it) {
         mIterator = it;
     }
--- src/no/ntnu/imt3281/sudoku/LockedCellException.java
+++ src/no/ntnu/imt3281/sudoku/LockedCellException.java
@@ -1,5 +1,10 @@
 package no.ntnu.imt3281.sudoku;
 
+/**
+ * LockedCellException is thrown when one tries to edit a locked sudoku cell.
+ *
+ * @see no.ntnu.imt3281.sudoku.Sudoku#setElement(int, int, int)
+ */
 @SuppressWarnings("serial")
 public class LockedCellException extends IllegalArgumentException {
 }
